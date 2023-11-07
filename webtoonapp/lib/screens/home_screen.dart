f6f2b928a63538b9ae1578190fab24c983184a74
5lft <zara0140@naver.com>
2023-11-07 18:21:06 +0900

    DetailScreen 생성 까지

--- webtoonapp/lib/screens/home_screen.dart
+++ webtoonapp/lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:webtoonapp/models/webtoon_model.dart';
 import 'package:webtoonapp/services/api_service.dart';
+import 'package:webtoonapp/widgets/webtoon_widget.dart';
 
 class HomeScreen extends StatelessWidget {
   HomeScreen({super.key});
@@ -9,7 +10,6 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    print(webtoons);
     return Scaffold(
       appBar: AppBar(
         elevation: 2,
@@ -28,22 +28,34 @@ class HomeScreen extends StatelessWidget {
         future: webtoons,
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            return ListView.separated(
-              // 인덱스가 처음엔 0부터 10까지만 호출됨 스크롤 하면 그 뒤에 필요한 아이템들이 만들어짐
-              scrollDirection: Axis.horizontal,
-              itemCount: snapshot.data!.length,
-              itemBuilder: (context, index) {
-                var webtoon = snapshot.data![index];
-                return Text(webtoon.title);
-              },
-              separatorBuilder: (context, index) => const SizedBox(width: 20),
+            return Column(
+              children: [
+                const SizedBox(
+                  height: 50,
+                ),
+                Expanded(child: makeList(snapshot))
+              ],
             );
           }
-          return const Center(
-            child: CircularProgressIndicator(),
-          );
+          return const Center();
         },
       ),
+    );
+  }
+
+  ListView makeList(AsyncSnapshot<List<WebtoonModel>> snapshot) {
+    return ListView.separated(
+      // 인덱스가 처음엔 0부터 10까지만 호출됨 스크롤 하면 그 뒤에 필요한 아이템들이 만들어짐
+      scrollDirection: Axis.horizontal,
+      itemCount: snapshot.data!.length,
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+      itemBuilder: (context, index) {
+        // 리스트뷰의 아이템을 빌드하는 함수. 파라미터로 받은 index 번째의 아이템을 빌드함
+        var webtoon = snapshot.data![index];
+        return Webtoon(
+            title: webtoon.title, thumb: webtoon.thumb, id: webtoon.id);
+      },
+      separatorBuilder: (context, index) => const SizedBox(width: 40),
     );
   }
 }
